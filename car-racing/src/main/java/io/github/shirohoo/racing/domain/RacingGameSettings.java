90c7e0cce91d0310670298c4b0d59a42c609fced
shirohoo <shirohoo4102@gmail.com>
2022-02-06 21:59:35 +0900

    :white_check_mark: RacingGameSettingsTests 구현
    Signed-off-by: shirohoo <shirohoo4102@gmail.com>

---
+++ car-racing/src/main/java/io/github/shirohoo/racing/domain/RacingGameSettings.java
@@ -0,0 +1,25 @@
+package io.github.shirohoo.racing.domain;
+
+import static java.util.Arrays.stream;
+import java.util.Objects;
+
+public record RacingGameSettings(String carNames, int tryCount, ForwardCondition condition) {
+    public RacingGameSettings {
+        assertThatNotNull(carNames, tryCount, condition);
+        assertThatConditions(carNames, tryCount);
+    }
+
+    public static RacingGameSettings of(String carNames, int tryCount, ForwardCondition condition) {
+        return new RacingGameSettings(carNames, tryCount, condition);
+    }
+
+    private void assertThatNotNull(Object... objects) {
+        stream(objects).forEach(Objects::requireNonNull);
+    }
+
+    private void assertThatConditions(String carNames, int tryCount) {
+        if (carNames.isBlank() || tryCount < 1) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
