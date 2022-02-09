aae81858fa1a89d0c7ab969f5abe94119444a34c
Changhun Han <shirohoo4102@gmail.com>
2022-02-09 14:13:15 +0900

    ✨ 숫자 야구 게임  (#9)
    * :sparkles: initialize
    
    * :fire: set .gitignore
    
    * :memo: 요구사항을 정의했다
    
    * :white_check_mark: RandomNumberGeneratorTests를 정의했다
    
    중복되지 않는 1~9사이의 랜덤한 수를 3개 추출하여 문자열로 반환한다
    
    * :sparkles: RandomNumberGeneratorTests를 구현했다
    
    중복되지 않는 1~9사이의 랜덤한 수를 3개 추출하여 문자열로 반환한다
    
    * :white_check_mark: NumbersTests를 정의했다
    
    숫자야구게임에 사용할 3자리의 숫자를 책임진다
    
    - 항상 유효한 상태(게임 룰 참고)로 생성된다
    - 중복되지 않는 1~9 사이의 숫자 3개를 입력받는다
    - 두개의 Numbers를 비교하여 볼, 스트라이크, 낫싱을 판별할 수 있어야 한다
    
    * :art: .gitignore를 재정의했다
    
    * :white_check_mark: UserTryTests를 정의했다
    
    숫자야구 게임에서 유저의 한번의 시도를 표현한다.
    유저의 입력을 받아 도메인에 전달한다.
    
    * :sparkles: UserTryTests를 구현했다
    
    숫자야구 게임에서 유저의 한번의 시도를 표현한다.
    유저의 입력을 받아 도메인에 전달한다.
    
    * :sparkles: Numbers를 구현했다
    
    * :sparkles: ConsoleOutput을 구현했다
    
    * :sparkles: ConsoleInput을 구현했다
    
    * :sparkles: ConsoleBaseBallApp을 구현했다
    
    * :memo: Update README.md
    
    * :art: gradle java 버전을 17로 고정했다

---
+++ base-ball/src/test/java/io/github/shirohoo/baseball/app/usecase/UserTryTests.java
@@ -0,0 +1,42 @@
+package io.github.shirohoo.baseball.app.usecase;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import io.github.shirohoo.baseball.app.port.in.TryCommand;
+import io.github.shirohoo.baseball.app.port.out.TryResult;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class UserTryTests {
+    @Test
+    void create() {
+        assertThatCode(UserTry::create)
+            .doesNotThrowAnyException();
+    }
+
+    @MethodSource
+    @ParameterizedTest
+    void trys(TryCommand userCommand, TryResult expected) {
+        UserTry game = UserTry.from(TryCommand.from("123"));
+
+        TryResult tryResult = game.trys(userCommand);
+
+        assertThat(tryResult).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> trys() {
+        return Stream.of(
+            Arguments.of(TryCommand.from("123"), TryResult.of(0, 3)),
+            Arguments.of(TryCommand.from("124"), TryResult.of(0, 2)),
+            Arguments.of(TryCommand.from("145"), TryResult.of(0, 1)),
+            Arguments.of(TryCommand.from("135"), TryResult.of(1, 1)),
+            Arguments.of(TryCommand.from("132"), TryResult.of(2, 1)),
+            Arguments.of(TryCommand.from("345"), TryResult.of(1, 0)),
+            Arguments.of(TryCommand.from("234"), TryResult.of(2, 0)),
+            Arguments.of(TryCommand.from("789"), TryResult.of(0, 0))
+        );
+    }
+}
