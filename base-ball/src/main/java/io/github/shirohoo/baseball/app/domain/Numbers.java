52fe27bbe7f36ec3a3074a1c5d74a30fc0a4d8bf
shirohoo <shirohoo4102@gmail.com>
2022-02-10 18:19:43 +0900

    :art: Numbers를 record로 변경했다
    Signed-off-by: shirohoo <shirohoo4102@gmail.com>

--- base-ball/src/main/java/io/github/shirohoo/baseball/app/domain/Numbers.java
+++ base-ball/src/main/java/io/github/shirohoo/baseball/app/domain/Numbers.java
@@ -1,12 +1,6 @@
 package io.github.shirohoo.baseball.app.domain;
 
-public class Numbers {
-    private final String numbers;
-
-    private Numbers(String numbers) {
-        this.numbers = numbers;
-    }
-
+public record Numbers(String numbers) {
     public static Numbers create(NumbersGenerativeStrategy strategy) {
         return new Numbers(strategy.generate());
     }
