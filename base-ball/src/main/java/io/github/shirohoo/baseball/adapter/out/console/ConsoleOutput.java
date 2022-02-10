a20305f9e9c591d7e04c11d17bd6a39f9dc1b646
shirohoo <shirohoo4102@gmail.com>
2022-02-10 18:36:31 +0900

    :sparkles: 콘솔 I/O 구현
    Signed-off-by: shirohoo <shirohoo4102@gmail.com>

---
+++ base-ball/src/main/java/io/github/shirohoo/baseball/adapter/out/console/ConsoleOutput.java
@@ -0,0 +1,49 @@
+package io.github.shirohoo.baseball.adapter.out.console;
+
+import io.github.shirohoo.baseball.app.domain.DecisionResult;
+
+public class ConsoleOutput {
+    private static final String ENTER_NUMBER_MESSAGE = "숫자를 입력해 주세요 : ";
+    private static final String BALL_MESSAGE = "볼";
+    private static final String STRIKE_MESSAGE = "스트라이크";
+    private static final String NOTHING_MESSAGE = "낫싱";
+    private static final String COMPLETE_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public void enterNumberMessage() {
+        print(ENTER_NUMBER_MESSAGE);
+    }
+
+    public void resultMessage(DecisionResult result) {
+        if (result.ballCount() == 0 && result.strikeCount() == 0) {
+            println(NOTHING_MESSAGE);
+        }
+        if (result.ballCount() > 0 && result.strikeCount() == 0) {
+            println(result.ballCount() + BALL_MESSAGE);
+        }
+        if (result.ballCount() == 0 && result.strikeCount() > 0) {
+            println(result.strikeCount() + STRIKE_MESSAGE);
+        }
+        if (result.ballCount() > 0 && result.strikeCount() > 0) {
+            println(String.format("%s%s %s%s",
+                result.ballCount(), BALL_MESSAGE,
+                result.strikeCount(), STRIKE_MESSAGE
+            ));
+        }
+        if (result.strikeCount() == 3) {
+            println(COMPLETE_MESSAGE);
+        }
+    }
+
+    public void restartMessage() {
+        println(RESTART_MESSAGE);
+    }
+
+    private void print(String message) {
+        System.out.print(message);
+    }
+
+    private void println(String message) {
+        System.out.println(message);
+    }
+}
