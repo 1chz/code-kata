41f26bc8d2074499dba8db0b6b59ca59ebf495db
shirohoo <shirohoo4102@gmail.com>
2022-02-10 18:13:14 +0900

    :sparkles: RandomNumbersGenerativeStrategy.generate 테스트 케이스를 구현했다
    Signed-off-by: shirohoo <shirohoo4102@gmail.com>

--- base-ball/src/main/java/io/github/shirohoo/baseball/app/domain/RandomNumbersGenerativeStrategy.java
+++ base-ball/src/main/java/io/github/shirohoo/baseball/app/domain/RandomNumbersGenerativeStrategy.java
@@ -1,4 +1,16 @@
 package io.github.shirohoo.baseball.app.domain;
 
+import static java.util.stream.Collectors.joining;
+import java.util.random.RandomGenerator;
+
 public class RandomNumbersGenerativeStrategy implements NumbersGenerativeStrategy {
+    private static final RandomGenerator RANDOM_GENERATOR = RandomGenerator.getDefault();
+
+    public String generate() {
+        return RANDOM_GENERATOR.ints(1, 10)
+            .distinct()
+            .limit(3)
+            .mapToObj(String::valueOf)
+            .collect(joining());
+    }
 }
