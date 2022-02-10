059a932bd05048dc66d843a9f2315ea2a86de7eb
shirohoo <shirohoo4102@gmail.com>
2022-02-10 18:20:39 +0900

    :white_check_mark: Numbers.nonOverlapping3digits 테스트 케이스를 정의했다
    Signed-off-by: shirohoo <shirohoo4102@gmail.com>

--- base-ball/src/main/java/io/github/shirohoo/baseball/app/domain/Numbers.java
+++ base-ball/src/main/java/io/github/shirohoo/baseball/app/domain/Numbers.java
@@ -4,4 +4,8 @@ public record Numbers(String numbers) {
     public static Numbers create(NumbersGenerativeStrategy strategy) {
         return new Numbers(strategy.generate());
     }
+
+    public static Numbers nonOverlapping3digits(String userInput) {
+        return null;
+    }
 }
--- base-ball/src/test/java/io/github/shirohoo/baseball/app/domain/NumbersTests.java
+++ base-ball/src/test/java/io/github/shirohoo/baseball/app/domain/NumbersTests.java
@@ -3,6 +3,9 @@ package io.github.shirohoo.baseball.app.domain;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class NumbersTests {
     @Test
@@ -18,5 +21,14 @@ class NumbersTests {
         assertThatThrownBy(() -> {
             Numbers randomNumbers = Numbers.create(null);
         }).isInstanceOf(NullPointerException.class);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"012", "890", "111", "112", "1234", "1111"})
+    void createExceptionWhenNot3NonOverlappingNumbersForUserInput(String userInput) {
+        assertThatThrownBy(() -> {
+            Numbers randomNumbers = Numbers.nonOverlapping3digits(userInput);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
