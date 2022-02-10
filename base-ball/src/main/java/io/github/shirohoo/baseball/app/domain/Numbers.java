907e070bd96a7cbe0fb8ccfceb338176006323a1
shirohoo <shirohoo4102@gmail.com>
2022-02-10 18:19:26 +0900

    :sparkles: Numbers.create를 마저 구현했다
    Signed-off-by: shirohoo <shirohoo4102@gmail.com>

--- base-ball/src/main/java/io/github/shirohoo/baseball/app/domain/Numbers.java
+++ base-ball/src/main/java/io/github/shirohoo/baseball/app/domain/Numbers.java
@@ -1,7 +1,13 @@
 package io.github.shirohoo.baseball.app.domain;
 
 public class Numbers {
+    private final String numbers;
+
+    private Numbers(String numbers) {
+        this.numbers = numbers;
+    }
+
     public static Numbers create(NumbersGenerativeStrategy strategy) {
-        return null;
+        return new Numbers(strategy.generate());
     }
 }
