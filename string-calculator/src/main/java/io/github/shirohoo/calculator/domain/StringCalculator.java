a60e8b1450d177ab823503e3c1b9c7003512d88b
Changhun Han <shirohoo4102@gmail.com>
2022-02-14 14:30:09 +0900

    ✨ 코드 카타 - 문자열 계산기  (#20)
    * :sparkles: 문자열 계산기 프로젝트 생성
    
    Signed-off-by: shirohoo <shirohoo4102@gmail.com>
    
    * :white_check_mark: ExpressionTests 유효성 검사 테스트 코드를 작성했다
    
    * :sparkles: ExpressionTests 유효성 검사 테스트 코드를 구현했다
    
    * :white_check_mark: ExpressionTests 수식 분리 테스트 코드를 작성했다
    
    * :sparkles: ExpressionTests 수식 분리 테스트 코드를 구현했다
    
    * :white_check_mark: StringCalculatorTests 초기화 테스트 코드를 작성했다
    
    * :sparkles: StringCalculatorTests 초기화 테스트 코드를 구현했다
    
    * :white_check_mark: StringCalculatorTests 계산 테스트 코드를 작성했다
    
    * :sparkles: StringCalculatorTests 계산 테스트 코드를 구현했다
    
    * :recycle: 리팩토링을 위해 ExpressionTests에 테스트 케이스를 몇 개 추가했다
    
    * :recycle: 리팩토링을 위해 ExpressionTests에 추가한 테스트 케이스를 구현했다
    
    * :recycle: StringCalculator 리팩토링
    
    * :memo: README.md를 작성했다

---
+++ string-calculator/src/main/java/io/github/shirohoo/calculator/domain/StringCalculator.java
@@ -0,0 +1,96 @@
+package io.github.shirohoo.calculator.domain;
+
+import static java.util.Arrays.stream;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.Stack;
+import java.util.function.DoubleBinaryOperator;
+
+public class StringCalculator {
+    private final Stack<String> accumulator;
+    private final Expression expression;
+
+    private StringCalculator(Expression expression) {
+        this.accumulator = new Stack<>();
+        this.expression = Objects.requireNonNull(expression);
+    }
+
+    public static StringCalculator from(Expression expression) {
+        return new StringCalculator(expression);
+    }
+
+    public double calculate() {
+        if (!expression.isSplit()) {
+            return expression.export();
+        }
+
+        accumulator.clear();
+        for (String expr : expression.split()) {
+            accumulateIfEqualSize3();
+            pushIfLessThanSize3(expr);
+        }
+
+        return Double.parseDouble(accumulate());
+    }
+
+    private void accumulateIfEqualSize3() {
+        if (accumulator.size() == 3) {
+            accumulate();
+        }
+    }
+
+    private String accumulate() {
+        double right = Double.parseDouble(accumulator.pop());
+        String operator = accumulator.pop();
+        double left = Double.parseDouble(accumulator.pop());
+
+        return accumulator.push(
+            ArithmeticCalculator.findBy(operator)
+                .applyAsDouble(left, right)
+        );
+    }
+
+    private void pushIfLessThanSize3(String expr) {
+        if (accumulator.size() < 3) {
+            accumulator.push(expr);
+        }
+    }
+
+    private enum ArithmeticCalculator {
+        ADDITION("+", (x, y) -> x + y),
+        SUBTRACTION("-", (x, y) -> x - y),
+        MULTIPLICATION("*", (x, y) -> x * y),
+        DIVISION("/", (x, y) -> x / y);
+
+        private static final Map<String, ArithmeticCalculator> MAP = stream(values()).collect(toMap(calculator -> calculator.operator, identity()));
+
+        private final String operator;
+        private final DoubleBinaryOperator function;
+
+        ArithmeticCalculator(String operator, DoubleBinaryOperator function) {
+            this.operator = operator;
+            this.function = function;
+        }
+
+        private static ArithmeticCalculator findBy(String operator) {
+            if (MAP.containsKey(operator)) {
+                return MAP.get(operator);
+            }
+            throw new NoSuchElementException("'%s' is not operator or not supported operator.".formatted(operator));
+        }
+
+        private String applyAsDouble(double left, double right) {
+            if (isDivideByZero(right)) {
+                throw new ArithmeticException("It cannot be divided by zero.");
+            }
+            return String.valueOf(function.applyAsDouble(left, right));
+        }
+
+        private boolean isDivideByZero(double right) {
+            return this == DIVISION && right == 0;
+        }
+    }
+}
---
+++ string-calculator/src/test/java/io/github/shirohoo/calculator/domain/StringCalculatorTests.java
@@ -0,0 +1,67 @@
+package io.github.shirohoo.calculator.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@DisplayName("문자열 계산기 테스트")
+class StringCalculatorTests {
+    @Test
+    @DisplayName("정상적인 수식이 입력되면 초기화에 성공한다")
+    void from() {
+        Expression expression = Expression.from("2 + 4 - 7 * 1 / 0");
+
+        assertThatCode(() -> {
+            StringCalculator calculator = StringCalculator.from(expression);
+        }).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("비정상적인 수식이 입력되면 초기화에 실패한다")
+    void fromException() {
+        assertThatThrownBy(() -> {
+            StringCalculator calculator = StringCalculator.from(null);
+        }).isInstanceOf(NullPointerException.class);
+    }
+
+    @MethodSource
+    @ParameterizedTest
+    @DisplayName("수식에 대한 순차적인 연산 결과를 반환한다")
+    void calculate(String expr, double expected) {
+        // ...given
+        Expression expression = Expression.from(expr);
+        StringCalculator calculator = StringCalculator.from(expression);
+
+        // ...when
+        double result = calculator.calculate();
+
+        // ...then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> calculate() {
+        return Stream.of(
+            Arguments.of("1", 1),
+            Arguments.of("5 - 1", 4),
+            Arguments.of("0 - 1", -1),
+            Arguments.of("2 + 4 - 1 * 5 / 5", 5),
+            Arguments.of("2 + 4 - 1 * 5 / 10", 2.5),
+            Arguments.of("100 + 100 - 5 * 2 / 3", 130),
+            Arguments.of("100 + 100 - 5 * 2 / 4", 97.5)
+        );
+    }
+
+    @Test
+    @DisplayName("0으로 나누려 하는 경우 예외가 발생한다")
+    void calculateDividedByZero() {
+        Expression expression = Expression.from("5 / 0");
+        StringCalculator calculator = StringCalculator.from(expression);
+        assertThatThrownBy(calculator::calculate).isInstanceOf(ArithmeticException.class);
+    }
+}
