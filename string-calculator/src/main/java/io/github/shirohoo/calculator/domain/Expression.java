5410b0d54bfb18124b4b29ba6266cb9675cee770
shirohoo <shirohoo4102@gmail.com>
2022-02-14 13:24:31 +0900

    :sparkles: ExpressionTests 유효성 검사 테스트 코드를 구현했다

---
+++ string-calculator/src/main/java/io/github/shirohoo/calculator/domain/Expression.java
@@ -0,0 +1,20 @@
+package io.github.shirohoo.calculator.domain;
+
+import java.util.regex.Pattern;
+
+public class Expression {
+    private static final Pattern PATTERN = Pattern.compile("^([0-9]*)|[0-9]*(\\s[+\\-*/]\\s[0-9].*)$");
+
+    private final String expr;
+
+    private Expression(String expr) {
+        if (expr == null || expr.isBlank() || !PATTERN.matcher(expr).matches()) {
+            throw new IllegalArgumentException("Expression must be space-separated and must be valid. for example, you cannot enter a expression such as 1 + +.");
+        }
+        this.expr = expr;
+    }
+
+    public static Expression from(String expr) {
+        return new Expression(expr);
+    }
+}
