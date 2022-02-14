aecda0429b3be61aa58a0869a37549760d6896c8
shirohoo <shirohoo4102@gmail.com>
2022-02-14 13:27:07 +0900

    :sparkles: ExpressionTests 수식 분리 테스트 코드를 구현했다

--- string-calculator/src/test/java/io/github/shirohoo/calculator/domain/ExpressionTests.java
+++ string-calculator/src/test/java/io/github/shirohoo/calculator/domain/ExpressionTests.java
@@ -1,7 +1,9 @@
 package io.github.shirohoo.calculator.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -18,5 +20,19 @@ class ExpressionTests {
         assertThatThrownBy(() -> {
             Expression expression = Expression.from(expr);
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("수식을 공백단위로 분리할 수 있어야 한다")
+    void split() {
+        // ...given
+        String expr = "2 + 4 - 7 * 1 / 0";
+        String[] expected = {"2", "+", "4", "-", "7", "*", "1", "/", "0"};
+
+        // ...when
+        Expression expression = Expression.from(expr);
+
+        // ...then
+        assertThat(expression.split()).isEqualTo(expected);
     }
 }
