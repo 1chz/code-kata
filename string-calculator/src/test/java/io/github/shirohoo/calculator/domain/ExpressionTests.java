3b0266dff7a06f24f7973da5019861235335281f
shirohoo <shirohoo4102@gmail.com>
2022-02-14 13:16:33 +0900

    :white_check_mark: ExpressionTests 유효성 검사 테스트 코드를 작성했다

---
+++ string-calculator/src/test/java/io/github/shirohoo/calculator/domain/ExpressionTests.java
@@ -0,0 +1,22 @@
+package io.github.shirohoo.calculator.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("수식 테스트")
+class ExpressionTests {
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {
+        "2+4-7*1/0", "5+7", "1 + +", "+", "-", "/", "*", "1 + +", "1 - -", "1 / /", "1 * *"
+    })
+    @DisplayName("유효하지 않은 수식이 입력되면 예외가 발생해야 한다")
+    void validate(String expr) {
+        assertThatThrownBy(() -> {
+            Expression expression = Expression.from(expr);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}
