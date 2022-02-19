de6ef3f4b5f8535e7c0be2c329d1a314127cafc7
Changhun Han <shirohoo4102@gmail.com>
2022-02-19 23:55:25 +0900

    :recycle: 누산기를 일급 컬렉션으로 리팩토링했다 (#22)
    Signed-off-by: shirohoo <shirohoo4102@gmail.com>

--- string-calculator/src/main/java/io/github/shirohoo/calculator/app/domain/StringCalculator.java
+++ string-calculator/src/main/java/io/github/shirohoo/calculator/app/domain/StringCalculator.java
@@ -2,7 +2,7 @@ package io.github.shirohoo.calculator.app.domain;
 
 import static java.util.Arrays.stream;
 import static java.util.function.Function.identity;
-import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toUnmodifiableMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Objects;
@@ -10,11 +10,11 @@ import java.util.Stack;
 import java.util.function.DoubleBinaryOperator;
 
 public class StringCalculator {
-    private final Stack<String> accumulator;
+    private final Accumulator accumulator;
     private final Expression expression;
 
     private StringCalculator(Expression expression) {
-        this.accumulator = new Stack<>();
+        this.accumulator = new Accumulator();
         this.expression = Objects.requireNonNull(expression);
     }
 
@@ -29,33 +29,50 @@ public class StringCalculator {
 
         accumulator.clear();
         for (String expr : expression.split()) {
-            accumulateIfEqualSize3();
-            pushIfLessThanSize3(expr);
-        }
-
-        return Double.parseDouble(accumulate());
-    }
-
-    private void accumulateIfEqualSize3() {
-        if (accumulator.size() == 3) {
-            accumulate();
-        }
-    }
-
-    private String accumulate() {
-        double right = Double.parseDouble(accumulator.pop());
-        String operator = accumulator.pop();
-        double left = Double.parseDouble(accumulator.pop());
-
-        return accumulator.push(
-            ArithmeticCalculator.findBy(operator)
-                .applyAsDouble(left, right)
-        );
-    }
-
-    private void pushIfLessThanSize3(String expr) {
-        if (accumulator.size() < 3) {
+            accumulator.accumulate();
             accumulator.push(expr);
+        }
+        accumulator.accumulate();
+
+        return Double.parseDouble(accumulator.pop());
+    }
+
+    private static class Accumulator {
+        private static final int STACK_SIZE = 3;
+
+        private final Stack<String> accumulator;
+
+        private Accumulator() {
+            accumulator = new Stack<>();
+            accumulator.setSize(STACK_SIZE);
+        }
+
+        private void clear() {
+            accumulator.clear();
+        }
+
+        private boolean isFull() {
+            return accumulator.size() == STACK_SIZE;
+        }
+
+        private void push(String ele) {
+            if (!isFull()) {
+                accumulator.push(ele);
+            }
+        }
+
+        private String pop() {
+            return accumulator.pop();
+        }
+
+        private void accumulate() {
+            if (isFull()) {
+                double right = Double.parseDouble(accumulator.pop());
+                String operator = accumulator.pop();
+                double left = Double.parseDouble(accumulator.pop());
+                accumulator.push(ArithmeticCalculator.findBy(operator)
+                    .applyAsDouble(left, right));
+            }
         }
     }
 
@@ -66,7 +83,7 @@ public class StringCalculator {
         DIVISION("/", (x, y) -> x / y);
 
         private static final Map<String, ArithmeticCalculator> MAP = stream(values())
-            .collect(toMap(ArithmeticCalculator::operator, identity()));
+            .collect(toUnmodifiableMap(ArithmeticCalculator::operator, identity()));
 
         private final String operator;
         private final DoubleBinaryOperator function;
