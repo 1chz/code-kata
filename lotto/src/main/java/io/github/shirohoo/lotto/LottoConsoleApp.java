4e2b2322a541c9e2c8711b7e83093aaf4f22d49c
shirohoo <shirohoo4102@gmail.com>
2022-03-03 21:02:44 +0900

    :sparkles: 로또 프로젝트 초기화
    Signed-off-by: shirohoo <shirohoo4102@gmail.com>

---
+++ lotto/src/main/java/io/github/shirohoo/lotto/LottoConsoleApp.java
@@ -0,0 +1,6 @@
+package io.github.shirohoo.lotto;
+
+public class LottoConsoleApp {
+    public static void main(String[] args) {
+    }
+}
---
+++ lotto/src/test/java/io/github/shirohoo/lotto/LottoConsoleAppTests.java
@@ -0,0 +1,9 @@
+package io.github.shirohoo.lotto;
+
+import org.junit.jupiter.api.Test;
+
+class LottoConsoleAppTests {
+    @Test
+    void contextLoads() {
+    }
+}
