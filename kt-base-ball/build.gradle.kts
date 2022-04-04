3c28bb0e74a23b0f3c8a140f715008661c2267c4
Changhun Han <shirohoo4102@gmail.com>
2022-04-04 22:05:06 +0900

    :fire: 프로젝트 재구성 (#28)
    * :sparkles: 코틀린 - 숫자야구게임 모듈 구성
    
    * :memo: 숫자 야구 게임 요구사항 및 기능 정의
    
    * :fire: 테스트 의존성 변경
    
    * :white_check_mark: 랜덤 숫자 생성기 테스트 코드 작성
    
    * :white_check_mark: 랜덤 숫자 생성기 테스트 코드 구현
    
    * :fire: 모든 모듈이 JVM 17을 사용하도록 수정
    
    * :fire: 블랙잭 제거

---
+++ kt-base-ball/build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.10"
+    application
+}
+
+group = "io.github.shirohoo"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ kt-base-ball/src/test/kotlin/io/github/shirohoo/baseball/domain/NumbersGeneratorTests.kt
@@ -0,0 +1,11 @@
+package io.github.shirohoo.baseball.domain
+
+import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.api.assertDoesNotThrow
+
+internal class NumbersGeneratorTests {
+    @RepeatedTest(10)
+    fun `1부터 9까지의 중복되지 않는 랜덤한 수 세개가 생성된다`() {
+        assertDoesNotThrow(NumbersGenerator::generate)
+    }
+}
