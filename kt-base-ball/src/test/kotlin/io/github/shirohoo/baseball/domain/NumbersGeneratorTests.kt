acf96117b257bed6a3308bd34595401a26047bf9
shirohoo <shirohoo4102@gmail.com>
2022-04-05 18:22:58 +0900

    :white_check_mark: Numbers#compare 테스트 코드 작성
    Signed-off-by: shirohoo <shirohoo4102@gmail.com>

--- kt-base-ball/src/test/kotlin/io/github/shirohoo/baseball/domain/NumbersGeneratorTests.kt
+++ kt-base-ball/src/test/kotlin/io/github/shirohoo/baseball/domain/NumbersGeneratorTests.kt
@@ -1,7 +1,13 @@
 package io.github.shirohoo.baseball.domain
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.of
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 internal class NumbersGeneratorTests {
     @RepeatedTest(10)
@@ -10,29 +16,24 @@ internal class NumbersGeneratorTests {
     }
 
     @ParameterizedTest
-    @MethodSource("data")
-    fun `컴퓨터의 수와 유저의 수를 비교한다`(Numbers userNumbers, MatchCounts expected) {
-        Numbers computerNumbers = Numbers . from ("123");
-
-        MatchCounts matchCounts = computerNumbers . compare (userNumbers);
-
-        assertThat(matchCounts).isEqualTo(expected);
+    @MethodSource("testCases")
+    fun `컴퓨터의 숫자와 유저의 숫자를 비교한다`(userNumbers: Numbers, expected: MatchCounts) {
+        val computerNumbers = Numbers("123")
+        val matchCounts = computerNumbers.compare(userNumbers)
+        assertEquals(expected, matchCounts)
     }
 
     companion object {
         @JvmStatic
-        fun Stream<Arguments> data ()
-        {
-            return Stream.of(
-                Arguments.of(Numbers("123"), MatchCounts(0, 3)),
-                Arguments.of(Numbers("124"), MatchCounts(0, 2)),
-                Arguments.of(Numbers("145"), MatchCounts(0, 1)),
-                Arguments.of(Numbers("135"), MatchCounts(1, 1)),
-                Arguments.of(Numbers("132"), MatchCounts(2, 1)),
-                Arguments.of(Numbers("345"), MatchCounts(1, 0)),
-                Arguments.of(Numbers("234"), MatchCounts(2, 0)),
-                Arguments.of(Numbers("789"), MatchCounts(0, 0))
-            )
-        }
+        fun testCases(): Stream<Arguments> = Stream.of(
+            of(Numbers("123"), MatchCounts(0, 3)),
+            of(Numbers("124"), MatchCounts(0, 2)),
+            of(Numbers("145"), MatchCounts(0, 1)),
+            of(Numbers("135"), MatchCounts(1, 1)),
+            of(Numbers("132"), MatchCounts(2, 1)),
+            of(Numbers("345"), MatchCounts(1, 0)),
+            of(Numbers("234"), MatchCounts(2, 0)),
+            of(Numbers("789"), MatchCounts(0, 0))
+        )
     }
 }
