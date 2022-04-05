0f34813a30aeda0273c8e92fccb6224c5e0a0f89
shirohoo <shirohoo4102@gmail.com>
2022-04-05 18:22:32 +0900

    :recycle: NumbersGenerator.kt
    Signed-off-by: shirohoo <shirohoo4102@gmail.com>

--- kt-base-ball/src/main/kotlin/io/github/shirohoo/baseball/domain/NumbersGenerator.kt
+++ kt-base-ball/src/main/kotlin/io/github/shirohoo/baseball/domain/NumbersGenerator.kt
@@ -1,18 +1,11 @@
 package io.github.shirohoo.baseball.domain
 
-import java.util.random.RandomGenerator
-import kotlin.streams.asSequence
-
 object NumbersGenerator {
     fun generate(): Numbers = Numbers(
-        generateRangeRandomSequence(1, 10)
+        generateSequence { (1..9).random() }
             .distinct()
             .take(3)
             .map(Int::toString)
-            .reduce { acc, string -> acc + string })
-
-    private fun generateRangeRandomSequence(start: Int, exclusiveEnd: Int): Sequence<Int> =
-        RandomGenerator.getDefault()
-            .ints(start, exclusiveEnd)
-            .asSequence()
+            .joinToString(separator = "")
+    )
 }
