e84888ceac63eef83e9bbbb5e9e32d585c82992e
shirohoo <shirohoo4102@gmail.com>
2022-04-06 11:39:59 +0900

    :sparkles: Console I/O

---
+++ kt-base-ball/src/main/kotlin/io/github/shirohoo/baseball/adapter/out/console/ConsoleOutAdapter.kt
@@ -0,0 +1,29 @@
+package io.github.shirohoo.baseball.adapter.out.console
+
+import io.github.shirohoo.baseball.domain.MatchCounts
+
+class ConsoleOutAdapter {
+    fun printInputGuide() {
+        print("숫자를 입력해 주세요 : ")
+    }
+
+    fun printToConsole(matchCounts: MatchCounts) {
+        println(getMessage(matchCounts))
+    }
+
+    fun printComplete() {
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+    }
+
+    private fun print(message: String): Unit = print(message)
+    private fun println(message: String): Unit = println(message)
+    private fun getMessage(matchCounts: MatchCounts): String = with(matchCounts) {
+        when {
+            (ballCount > 0) and (strikeCount > 0) -> "${ballCount}볼 ${strikeCount}스트라이크"
+            (ballCount > 0) and (strikeCount == 0) -> "${ballCount}볼"
+            (ballCount == 0) and (strikeCount > 0) -> "${strikeCount}스트라이크"
+            (ballCount == 0) and (strikeCount == 0) -> "낫싱"
+            else -> "${strikeCount}스트라이크"
+        }
+    }
+}
