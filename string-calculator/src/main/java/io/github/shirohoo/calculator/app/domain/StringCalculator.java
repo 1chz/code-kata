bedf5fea10dfd87b14ccc9bd72e54beef31714da
shirohoo <shirohoo4102@gmail.com>
2022-04-27 20:48:12 +0900

    :recycle: StringCalculator 리팩토링

--- string-calculator/src/main/java/io/github/shirohoo/calculator/app/domain/StringCalculator.java
+++ string-calculator/src/main/java/io/github/shirohoo/calculator/app/domain/StringCalculator.java
@@ -1,23 +1,20 @@
 package io.github.shirohoo.calculator.app.domain;
 
-import java.util.Deque;
 import java.util.Queue;
 
 @SuppressWarnings("all") // will never happen
 public final class StringCalculator {
     public double calculate(StringExpression expr) {
-        Deque<Double> operands = expr.getOperands();
+        Queue<Double> operands = expr.getOperands();
         Queue<Character> operators = expr.getOperators();
+
+        double result = operands.poll();
 
         while (operators.size() > 0) {
             ArithmeticOperator operator = ArithmeticOperator.findByOperator(operators.poll());
-            double left = operands.pollFirst();
-            double right = operands.pollFirst();
-
-            double calculationResult = operator.apply(left, right);
-            operands.offerFirst(calculationResult);
+            result = operator.apply(result, operands.poll());
         }
 
-        return operands.poll();
+        return result;
     }
 }
