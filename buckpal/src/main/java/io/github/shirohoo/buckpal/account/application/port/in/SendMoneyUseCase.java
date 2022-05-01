dd01738ee9bb783d5157e91cbac2d76500a37543
shirohoo <shirohoo4102@gmail.com>
2022-05-01 23:57:48 +0900

    :recycle: 일부 모듈 제거 및 문서 최신화
    - buckpal 제거
    - 볼링게임 제거

--- buckpal/src/main/java/io/github/shirohoo/buckpal/account/application/port/in/SendMoneyUseCase.java
+++
@@ -1,7 +0,0 @@
-package io.github.shirohoo.buckpal.account.application.port.in;
-
-public interface SendMoneyUseCase {
-
-    boolean sendMoney(SendMoneyCommand command);
-
-}
--- buckpal/src/main/java/io/github/shirohoo/buckpal/account/application/port/out/UpdateAccountStatePort.java
+++
@@ -1,9 +0,0 @@
-package io.github.shirohoo.buckpal.account.application.port.out;
-
-import io.github.shirohoo.buckpal.account.application.domain.Account;
-
-public interface UpdateAccountStatePort {
-
-    void updateActivities(Account account);
-
-}
