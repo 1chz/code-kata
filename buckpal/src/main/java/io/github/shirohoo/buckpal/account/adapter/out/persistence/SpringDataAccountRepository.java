c0c594dbeadc0483f5792a192454959a35836d82
Changhun Han <shirohoo4102@gmail.com>
2022-05-01 23:58:36 +0900

    :recycle: 일부 모듈 제거 및 문서 최신화 (#36)
    - buckpal 제거
    - 볼링게임 제거

--- buckpal/src/main/java/io/github/shirohoo/buckpal/account/adapter/out/persistence/SpringDataAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.shirohoo.buckpal.account.adapter.out.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface SpringDataAccountRepository extends JpaRepository<AccountJpaEntity, Long> {
-
-}
