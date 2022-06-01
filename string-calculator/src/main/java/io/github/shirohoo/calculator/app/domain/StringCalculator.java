c3f0910de650d14edbd592ae627d9162c31a500a
Changhun Han <shirohoo4102@gmail.com>
2022-06-01 11:32:08 +0900

    :recycle: StringCalculator.java

--- string-calculator/src/main/java/io/github/shirohoo/calculator/app/domain/StringCalculator.java
+++ string-calculator/src/main/java/io/github/shirohoo/calculator/app/domain/StringCalculator.java
@@ -1,16 +1,24 @@
 package io.github.shirohoo.calculator.app.domain;
 
-import static io.github.shirohoo.calculator.app.domain.ArithmeticOperator.findByOperator;
 import java.util.Queue;
+import java.util.function.BinaryOperator;
 
-@SuppressWarnings("all") // will never happen
+@SuppressWarnings("all") // there will never be a problem
 public final class StringCalculator {
     public double calculate(StringExpression expr) {
         Queue<Double> operands = expr.getOperands();
         Queue<Character> operators = expr.getOperators();
 
         return operands.stream()
-            .reduce((left, right) -> findByOperator(operators.poll()).apply(left, right))
-            .orElseThrow();
+                .reduce(accumulate(operators))
+                .orElseThrow();
+    }
+
+    private BinaryOperator<Double> accumulate(Queue<Character> operators) {
+        return (first, last) -> {
+            Character operator = operators.poll();
+            ArithmeticOperator arithmeticOperator = ArithmeticOperator.findByOperator(operator);
+            return arithmeticOperator.apply(first, last);
+        };
     }
 }
