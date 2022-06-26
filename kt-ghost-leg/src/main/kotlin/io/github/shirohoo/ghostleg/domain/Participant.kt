7378dd136e8dd8144d8ef18b21b38d6a589f156f
shirohoo <shirohoo4102@gmail.com>
2022-06-26 12:39:45 +0900

    :recycle: Participant constructor and convention

--- kt-ghost-leg/src/main/kotlin/io/github/shirohoo/ghostleg/domain/Participant.kt
+++ kt-ghost-leg/src/main/kotlin/io/github/shirohoo/ghostleg/domain/Participant.kt
@@ -1,7 +1,9 @@
 package io.github.shirohoo.ghostleg.domain
 
-class Participant(val name: Name, val point: Point) {
+class Participant(val name: Name, val point: Point = Point()) {
     val x = point.x
+
     val y = point.y
+
     fun move(dx: Int, dy: Int): Participant = Participant(name, point.move(dx, dy))
 }
