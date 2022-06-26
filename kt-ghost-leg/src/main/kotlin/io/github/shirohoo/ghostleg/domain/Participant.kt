58f67fb816be6eea9b94ade423ad0b15f3acc472
shirohoo <shirohoo4102@gmail.com>
2022-06-26 14:36:29 +0900

    :recycle: change participant class to data class
    need equals and hash code

--- kt-ghost-leg/src/main/kotlin/io/github/shirohoo/ghostleg/domain/Participant.kt
+++ kt-ghost-leg/src/main/kotlin/io/github/shirohoo/ghostleg/domain/Participant.kt
@@ -1,6 +1,6 @@
 package io.github.shirohoo.ghostleg.domain
 
-class Participant(val name: Name, val point: Point = Point()) {
+data class Participant(val name: Name, val point: Point = Point()) {
     constructor(name: String) : this(Name(name))
 
     val x = point.x
