dbed3d19866f0775190a04ce98b3b6d6b5893757
shirohoo <shirohoo4102@gmail.com>
2022-06-30 20:13:50 +0900

    :sparkles: added test case for Participants class and passed it

--- kt-ghost-leg/src/main/kotlin/io/github/shirohoo/ghostleg/domain/Participants.kt
+++ kt-ghost-leg/src/main/kotlin/io/github/shirohoo/ghostleg/domain/Participants.kt
@@ -1,9 +1,19 @@
 package io.github.shirohoo.ghostleg.domain
 
 class Participants(vararg participants: Participant) {
-    constructor(vararg participants: String) : this(*participants.map(::Participant).toTypedArray())
-
     private val participants: Array<Participant> = arrayOf(*participants)
 
     val size = this.participants.size
+
+    init {
+        require(participants.size >= MIN_SIZE) {
+            "the number of participants must be at least 2."
+        }
+    }
+
+    constructor(vararg participants: String) : this(*participants.map(::Participant).toTypedArray())
+
+    companion object {
+        private const val MIN_SIZE = 2
+    }
 }
--- kt-ghost-leg/src/test/kotlin/io/github/shirohoo/ghostleg/domain/ParticipantsTests.kt
+++ kt-ghost-leg/src/test/kotlin/io/github/shirohoo/ghostleg/domain/ParticipantsTests.kt
@@ -2,6 +2,7 @@ package io.github.shirohoo.ghostleg.domain
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class ParticipantsTests {
     @Test
@@ -23,5 +24,10 @@ class ParticipantsTests {
         val participants = Participants("james", "john", "anna")
 
         assertEquals(3, participants.size)
+    }
+
+    @Test
+    fun `참가자는 최소한 두명 이상이어야 한다`() {
+        assertThrows<IllegalArgumentException> { Participants("james") }
     }
 }
