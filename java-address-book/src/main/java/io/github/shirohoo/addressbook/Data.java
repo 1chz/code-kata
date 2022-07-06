5709f0a3387021e96a40b63b8fc504af768e8bce
shirohoo <shirohoo4102@gmail.com>
2022-07-06 22:10:44 +0900

    :sparkles: 데이터 테스트 코드 작성 및 구현

---
+++ java-address-book/src/main/java/io/github/shirohoo/addressbook/Data.java
@@ -0,0 +1,108 @@
+package io.github.shirohoo.addressbook;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public final class Data {
+    public final String id;
+    public final String name;
+    public final String gender;
+    public final int age;
+    public final String phoneNumber;
+    public final String address;
+
+    private Data(
+            String id,
+            String name,
+            String gender,
+            int age,
+            String phoneNumber,
+            String address
+    ) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public static DataBuilder builder() {
+        return new DataBuilder();
+    }
+
+    @Override
+    public String toString() {
+        return "Data{id='%s', name='%s', gender='%s', age=%d, phoneNumber='%s', address='%s'}"
+                .formatted(id, name, gender, age, phoneNumber, address);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Data that = (Data) o;
+        return age == that.age &&
+                Objects.equals(id, that.id) &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(gender, that.gender) &&
+                Objects.equals(phoneNumber, that.phoneNumber) &&
+                Objects.equals(address, that.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, gender, age, phoneNumber, address);
+    }
+
+    public final static class DataBuilder {
+        private String id;
+        private String name;
+        private String gender;
+        private int age;
+        private String phoneNumber;
+        private String address;
+
+        public DataBuilder id(UUID id) {
+            if (id == null) {
+                this.id = UUID.randomUUID().toString();
+                return this;
+            }
+            this.id = id.toString();
+            return this;
+        }
+
+        public DataBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public DataBuilder gender(String gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public DataBuilder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public DataBuilder phoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public DataBuilder address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Data build() {
+            return new Data(id, name, gender, age, phoneNumber, address);
+        }
+    }
+}
---
+++ java-address-book/src/test/java/io/github/shirohoo/addressbook/DataTests.java
@@ -0,0 +1,25 @@
+package io.github.shirohoo.addressbook;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+
+class DataTests {
+    @Test
+    void build() {
+        Data data = Data.builder()
+                .id(UUID.randomUUID())
+                .name("siro")
+                .gender("M")
+                .age(28)
+                .phoneNumber("010-1234-5678")
+                .address("Korea")
+                .build();
+
+        assertEquals("siro", data.name);
+        assertEquals("M", data.gender);
+        assertEquals(28, data.age);
+        assertEquals("010-1234-5678", data.phoneNumber);
+        assertEquals("Korea", data.address);
+    }
+}
