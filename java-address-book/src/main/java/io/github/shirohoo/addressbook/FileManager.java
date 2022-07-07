755981300d2cbe0c0b9da97060ddd78292524895
shirohoo <shirohoo4102@gmail.com>
2022-07-07 21:56:08 +0900

    :recycle: 파일관리자가 생성자로 root path를 받도록 리팩토링

--- java-address-book/src/main/java/io/github/shirohoo/addressbook/FileManager.java
+++ java-address-book/src/main/java/io/github/shirohoo/addressbook/FileManager.java
@@ -1,16 +1,16 @@
 package io.github.shirohoo.addressbook;
 
-import static java.util.stream.Collectors.joining;
 import java.io.File;
+import java.nio.file.Path;
 
 final class FileManager {
-    String getFilePath(String hash) {
-        return "%s%s%s".formatted(pathFrom(hash), File.separator, hash);
+    private final Path rootPath;
+
+    public FileManager(Path root) {
+        this.rootPath = root;
     }
 
-    private String pathFrom(String hash) {
-        return hash.chars()
-                .mapToObj(String::valueOf)
-                .collect(joining(File.separator));
+    String getFilePath(String hash) {
+        return rootPath.toString() + File.separator + hash;
     }
 }
--- java-address-book/src/test/java/io/github/shirohoo/addressbook/FileManagerTests.java
+++ java-address-book/src/test/java/io/github/shirohoo/addressbook/FileManagerTests.java
@@ -1,23 +1,33 @@
 package io.github.shirohoo.addressbook;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.junit.jupiter.api.Test;
 
 class FileManagerTests {
     @Test
     void getFilePath() {
         // given
-        FileManager fileManager = new FileManager();
+        Path root = Paths.get("src", "main", "resources", "addressbook");
+        FileManager fileManager = new FileManager(root);
         String hash = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824";
 
         // when
         String filePath = fileManager.getFilePath(hash);
 
         // then
-        assertEquals("50\\99\\102\\50\\52\\100\\98\\97\\53\\102\\98\\48\\97\\51\\48\\101"
-                + "\\50\\54\\101\\56\\51\\98\\50\\97\\99\\53\\98\\57\\101\\50\\57\\101\\49\\98\\49"
-                + "\\54\\49\\101\\53\\99\\49\\102\\97\\55\\52\\50\\53\\101\\55\\51\\48\\52\\51\\51"
-                + "\\54\\50\\57\\51\\56\\98\\57\\56\\50\\52\\2cf24dba5fb0a30e26e83b2ac5b9e29e1b161"
-                + "e5c1fa7425e73043362938b9824", filePath);
+        String expected = "src"
+                + File.separator
+                + "main"
+                + File.separator
+                + "resources"
+                + File.separator
+                + "addressbook"
+                + File.separator
+                + "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824";
+
+        assertEquals(expected, filePath);
     }
 }
