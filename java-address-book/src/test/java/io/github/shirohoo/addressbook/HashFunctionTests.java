24338d74420209b1b2cb0f217d1cb212f5700930
Changhun Han <shirohoo4102@gmail.com>
2022-07-08 14:47:09 +0900

    :recycle: 테스트 수정

--- java-address-book/src/test/java/io/github/shirohoo/addressbook/HashFunctionTests.java
+++ java-address-book/src/test/java/io/github/shirohoo/addressbook/HashFunctionTests.java
@@ -2,13 +2,12 @@ package io.github.shirohoo.addressbook;
 
 import org.junit.jupiter.api.Test;
 
-import static io.github.shirohoo.addressbook.HashFunction.hashing;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class HashFunctionTests {
     @Test
-    void digest() {
-        String hashcode = hashing("hello");
-        assertEquals("2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824", hashcode);
+    void hashing() {
+        String hashcode = HashFunction.hashing("hello");
+        assertTrue(hashcode.matches("^[0-9a-f]{64}$"));
     }
 }
