695a4607f3c152f2d54df7114303cc1769104faf
shirohoo <shirohoo4102@gmail.com>
2022-07-20 10:20:00 +0900

    :recycle: 재귀함수를 사용하도록 내부 구현을 리팩토링

--- java-string-calculator/src/main/java/io/github/shirohoo/stringcalculator/StringCalculator.java
+++ java-string-calculator/src/main/java/io/github/shirohoo/stringcalculator/StringCalculator.java
@@ -1,17 +1,35 @@
 package io.github.shirohoo.stringcalculator;
 
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.function.BinaryOperator;
 
 @SuppressWarnings("all") // there will never be a problem
 public final class StringCalculator {
     public double calculate(StringExpression expr) {
-        Queue<Double> operands = expr.getOperands();
-        Queue<Character> operators = expr.getOperators();
+        return calculate(expr.splitExpression());
+//
+//        Queue<Double> operands = expr.getOperands();
+//        Queue<Character> operators = expr.getOperators();
+//
+//        return operands.stream()
+//                .reduce(accumulate(operators))
+//                .orElseThrow();
+    }
 
-        return operands.stream()
-                .reduce(accumulate(operators))
-                .orElseThrow();
+    private double calculate(ArrayDeque<String> queue) {
+        if (queue.size() == 1) {
+            return Double.parseDouble(queue.removeFirst());
+        }
+
+        double left = Double.parseDouble(queue.removeFirst());
+        ArithmeticOperator operator = ArithmeticOperator.findByOperator(queue.removeFirst().charAt(0));
+        double right = Double.parseDouble(queue.removeFirst());
+        double result = operator.apply(left, right);
+
+        queue.addFirst(String.valueOf(result));
+
+        return calculate(queue);
     }
 
     private BinaryOperator<Double> accumulate(Queue<Character> operators) {
