57b00be90726c5fe8e8c5bc7b9736e813b5e2523
shirohoo <shirohoo4102@gmail.com>
2022-07-24 15:43:00 +0900

    :sparkles: wrote AlarmsTests and implement

---
+++ java-alarm-clock/src/main/java/io/github/shirohoo/Alarms.java
@@ -0,0 +1,37 @@
+package io.github.shirohoo;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+final class Alarms {
+    private final Set<Time> times;
+
+    public Alarms() {
+        this(new CopyOnWriteArraySet<>());
+    }
+
+    public Alarms(Set<Time> times) {
+        this.times = times;
+    }
+
+    public void registry(Time time) {
+        try {
+            times.add(time);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            // do nothing...
+        }
+
+        System.out.println("Current alarms = " + times);
+    }
+
+    public Optional<Time> timeFor(Time time) {
+        synchronized (time) {
+            if (times.removeIf(it -> it.equals(time))) {
+                return Optional.of(time);
+            }
+            return Optional.empty();
+        }
+    }
+}
---
+++ java-alarm-clock/src/test/java/io/github/shirohoo/AlarmsTests.java
@@ -0,0 +1,31 @@
+package io.github.shirohoo;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AlarmsTests {
+
+    @Test
+    void shouldAbleAddToTasks() {
+        Alarms sut = new Alarms();
+
+        assertDoesNotThrow(() -> sut.registry(new Time("12:00:00")));
+    }
+
+    @Test
+    void shouldRemovedAlarmTask() {
+        // given
+        Alarms sut = new Alarms();
+        sut.registry(new Time("12:00:00"));
+
+        // when
+        Optional<Time> actual = sut.timeFor(new Time("12:00:00"));
+
+        // then
+        assertTrue(actual.isPresent());
+    }
+}
