72ff3bec5142e586958d6304382674009310cf9f
shirohoo <shirohoo4102@gmail.com>
2022-07-24 15:41:35 +0900

    :recycle: Time and TimeTests

--- java-alarm-clock/src/main/java/io/github/shirohoo/Time.java
+++ java-alarm-clock/src/main/java/io/github/shirohoo/Time.java
@@ -3,10 +3,13 @@ package io.github.shirohoo;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
-public record Time(String time) {
+record Time(String time) {
     public Time {
-        // verify
-        LocalTime.parse(time, DateTimeFormatter.ofPattern("H:mm:ss"));
+        try {
+            LocalTime.parse(time, DateTimeFormatter.ofPattern("H:mm:ss"));
+        } catch (Exception e) {
+            throw new IllegalArgumentException("입력 시간 형식이 hh:mm:ss가 아닙니다.");
+        }
     }
 
     @Override
--- java-alarm-clock/src/test/java/io/github/shirohoo/TimeTests.java
+++ java-alarm-clock/src/test/java/io/github/shirohoo/TimeTests.java
@@ -2,19 +2,19 @@ package io.github.shirohoo;
 
 import org.junit.jupiter.api.Test;
 
-import java.time.format.DateTimeParseException;
-
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TimeTests {
     @Test
-    void shouldThrownFormattingException() {
-        assertThrows(DateTimeParseException.class, () -> new Time("120000"));
+    void shouldThrownDateTimeParseException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Time("120000"),
+                "입력 시간 형식이 hh:mm:ss가 아닙니다.");
     }
 
     @Test
-    void shouldReturnTrue() {
+    void shouldReturnEqualsTrue() {
         Time time = new Time("12:00:00");
         Time other = new Time("12:00:00");
 
