37371e2d56cbf4a62e9e693ebbbf81b6d4f035e0
shirohoo <shirohoo4102@gmail.com>
2022-07-24 15:43:14 +0900

    :sparkles: wrote AlarmExecutor

---
+++ java-alarm-clock/src/main/java/io/github/shirohoo/AlarmExecutor.java
@@ -0,0 +1,18 @@
+package io.github.shirohoo;
+
+final class AlarmExecutor extends Thread {
+    private final Alarms alarms;
+
+    public AlarmExecutor(Alarms alarms) {
+        this.alarms = alarms;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            alarms.timeFor(Clock.currentTime()).ifPresent(it -> {
+                System.out.println("beep beep beep beep beep");
+            });
+        }
+    }
+}
